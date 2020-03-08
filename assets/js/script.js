f71b7c94751ed85445232cc0a51353f5dfa7fc40
hana <naritakanako@dande-lion.co.jp>
2020-03-08 14:41:36 +0900

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,83 +39,83 @@ $(function () {
   });
 });
 
-//すらいだー//
-$(function () {
-  //（１）ページの概念・初期ページを設定
-  var page = 0;
+// //すらいだー//
+// $(function () {
+//   //（１）ページの概念・初期ページを設定
+//   var page = 0;
 
-  //（２）イメージの数を最後のページ数として変数化
-  var lastPage = parseInt($(".slide img").length - 1);
+//   //（２）イメージの数を最後のページ数として変数化
+//   var lastPage = parseInt($(".slide img").length - 1);
 
-  //（３）最初に全部のイメージを一旦非表示にします
-  $(".slide img").css("display", "none");
+//   //（３）最初に全部のイメージを一旦非表示にします
+//   $(".slide img").css("display", "none");
 
-  //（４）初期ページを表示
-  $(".slide img").eq(page).css("display", "block");
+//   //（４）初期ページを表示
+//   $(".slide img").eq(page).css("display", "block");
 
-  //（５）ページ切換用、自作関数作成
-  function changePage() {
-    $(".slide img").fadeOut(1000);
-    $(".slide img").eq(page).fadeIn(1000);
-  };
+//   //（５）ページ切換用、自作関数作成
+//   function changePage() {
+//     $(".slide img").fadeOut(1000);
+//     $(".slide img").eq(page).fadeIn(1000);
+//   };
 
-  //（６）～秒間隔でイメージ切換の発火設定
-  var Timer;
-  function startTimer() {
-    Timer = setInterval(function () {
-      if (page === lastPage) {
-        page = 0;
-        changePage();
-      } else {
-        page++;
-        changePage();
-      };
-    }, 5000);
-  }
-  //（７）～秒間隔でイメージ切換の停止設定
-  function stopTimer() {
-    clearInterval(Timer);
-  }
+//   //（６）～秒間隔でイメージ切換の発火設定
+//   var Timer;
+//   function startTimer() {
+//     Timer = setInterval(function () {
+//       if (page === lastPage) {
+//         page = 0;
+//         changePage();
+//       } else {
+//         page++;
+//         changePage();
+//       };
+//     }, 5000);
+//   }
+//   //（７）～秒間隔でイメージ切換の停止設定
+//   function stopTimer() {
+//     clearInterval(Timer);
+//   }
 
-  //（８）タイマースタート
-  startTimer();
+//   //（８）タイマースタート
+//   startTimer();
 
-  /*オプションを足す場合はここへ記載*/
-  //（９）「次へ」をクリック
-  $(".nav-r a").click(function () {
-    //タイマー停止＆スタート（クリックした時点から～秒とする為）
-    stopTimer();
-    startTimer();
-    if (page === lastPage) {
-      page = 0;
-      changePage();
-    } else {
-      page++;
-      changePage();
-    };
-  });
-  //「戻る」をクリック
-  $(".nav-l a").click(function () {
-    //タイマー停止＆スタート（クリックした時点から～秒とする為）
-    stopTimer();
-    startTimer();
-    if (page === 0) {
-      page = lastPage;
-      changePage();
-    } else {
-      page--;
-      changePage();
-    };
-  });
+//   /*オプションを足す場合はここへ記載*/
+//   //（９）「次へ」をクリック
+//   $(".nav-r a").click(function () {
+//     //タイマー停止＆スタート（クリックした時点から～秒とする為）
+//     stopTimer();
+//     startTimer();
+//     if (page === lastPage) {
+//       page = 0;
+//       changePage();
+//     } else {
+//       page++;
+//       changePage();
+//     };
+//   });
+//   //「戻る」をクリック
+//   $(".nav-l a").click(function () {
+//     //タイマー停止＆スタート（クリックした時点から～秒とする為）
+//     stopTimer();
+//     startTimer();
+//     if (page === 0) {
+//       page = lastPage;
+//       changePage();
+//     } else {
+//       page--;
+//       changePage();
+//     };
+//   });
 
-});
+// });
 
 
 //swipe
 $(function () {
   $(".swipeshow").swipeshow({
     autostart: true, // 自動スタートするか否か。する場合はtrue、しない場合はfalse
-    interval: 3000, // スライド切替のインターバル
+    //interval: 1000, // スライド切替のインターバル
     initial: 0, // スライドインデックス
     speed: 1000, // アニメーションスピード
     friction: 0.3, // バウンスバック動作
@@ -172,3 +172,28 @@ $(function () {
   });
 });
 
+//星がクリックされたら、何番目の星がクリックされたかを判定し、input[type=range]のvalue属性に設定
+$(function() {
+  $(".range-group").each(function() {
+    for (var i = 0; i < 5; i++) {
+      $(this).append("<a>");
+    }
+  });
+  $(".range-group>a").on("click", function() {
+    var index = $(this).index();
+    $(this)
+      .siblings()
+      .removeClass("on");
+    for (var i = 0; i < index; i++) {
+      $(this)
+        .parent()
+        .find("a")
+        .eq(i)
+        .addClass("on");
+    }
+    $(this)
+      .parent()
+      .find(".input-range")
+      .attr("value", index);
+  });
+});
